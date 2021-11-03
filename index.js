656a7b92e674f6e13f98d9e75b337ca1d3232fd2
Samuel Ramos <samuelramox@outlook.com>
2021-11-03 20:36:33 -0300

    refactor: organize files

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const express = require('express');
-const consign = require('consign');
+const customExpress = require('./config/customExpress');
 
-const app = express();
-
-consign().include('controllers').into(app);
+const app = customExpress();
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
