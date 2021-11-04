67063bc60c6709f4ba486461446fa4614f0f8027
Samuel Ramos <samuelramox@outlook.com>
2021-11-04 00:27:09 -0300

    feat: update sql command

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -6,8 +6,7 @@ class Tabelas {
   }
 
   criarAtendimento() {
-    const sql =
-      'CREATE TABLE Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))';
+    const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))';
 
     return this.conexao.query(sql, (erro) => {
       if (erro) {
