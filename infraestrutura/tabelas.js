a0f2185a870a68d99020444f77ce90838ce588e1
Samuel Ramos <samuelramox@outlook.com>
2021-11-04 22:02:21 -0300

    feat: update table with dates

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -6,7 +6,8 @@ class Tabelas {
   }
 
   criarAtendimento() {
-    const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))';
+    const sql =
+      'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))';
 
     return this.conexao.query(sql, (erro) => {
       if (erro) {
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,20 @@
+const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
   adiciona(atendimento) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format(
+      'YYYY-MM-DD HH:mm:ss'
+    );
+    const atendimentoDatado = {
+      ...atendimento,
+      dataCriacao,
+      data,
+    };
     const sql = 'INSERT INTO atendimentos SET ?';
 
-    conexao.query(sql, atendimento, (erro, resultados) => {
+    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
       if (erro) {
         throw erro;
       } else {
