9d67544b33347b06809885ad56a48f3c1e96d539
Samuel Ramos <samuelramox@outlook.com>
2021-11-04 22:37:01 -0300

    feat: add values validate

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,20 +7,43 @@ class Atendimento {
     const data = moment(atendimento.data, 'DD/MM/YYYY').format(
       'YYYY-MM-DD HH:mm:ss'
     );
-    const atendimentoDatado = {
-      ...atendimento,
-      dataCriacao,
-      data,
-    };
-    const sql = 'INSERT INTO atendimentos SET ?';
 
-    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-      if (erro) {
-        res.status(400).json(erro);
-      } else {
-        res.status(201).json(resultados);
-      }
-    });
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 5;
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual a data atual',
+      },
+      {
+        nome: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'Cliente deve ter pelo menos cinco caracteres',
+      },
+    ];
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
+
+    if (existemErros) {
+      res.status(400).json(erros);
+    } else {
+      const atendimentoDatado = {
+        ...atendimento,
+        dataCriacao,
+        data,
+      };
+
+      const sql = 'INSERT INTO atendimentos SET ?';
+
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if (erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(resultados);
+        }
+      });
+    }
   }
 }
 
